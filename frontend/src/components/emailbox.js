e0c369186d19e1bf7f2ad422fea262297386273a
Raxy777 <ramanathrakshit@gmail.com>
2025-03-27 07:57:40 +0530

    made details required in contact us page

--- frontend/src/components/emailbox.js
+++ frontend/src/components/emailbox.js
@@ -35,11 +35,11 @@ export const ContactUs = () => {
     <div>
       <form ref={form} onSubmit={sendEmail}>
         <label>Name</label>
-        <input type="text" name="from_name" />
+        <input type="text" name="from_name" required />
         <label>Email</label>
-        <input type="email" name="user_email" />
+        <input type="email" name="user_email" required />
         <label>Message</label>
-        <textarea name="message" />
+        <textarea name="message" required />
         <input type="submit" value="Send" className="submit" id="liveToastBtn" />
       </form>
 
